92c1096b54b04402e5a9e204a39ce12a22fd715f
Tony Young <tony@rfw.name>
2014-02-03 12:55:06 +1300

    [services/admin] add code ported from tornado

--- kochira/services/admin.py
+++ kochira/services/admin.py
@@ -1,5 +1,6 @@
 from io import StringIO
 import os
+import signal
 import subprocess
 import sys
 
@@ -179,8 +180,48 @@ def rehash(client, target, origin):
 def restart(client, target, origin):
     for client in list(client.bot.networks.values()):
         client.quit("Restarting...")
-    os.execvp(os.path.join(sys.path[0], sys.argv[0]), sys.argv)
 
+    # The following code is ported from Tornado.
+    # http://www.tornadoweb.org/en/branch2.4/_modules/tornado/autoreload.html
+
+    if hasattr(signal, "setitimer"):
+        # Clear the alarm signal set by
+        # ioloop.set_blocking_log_threshold so it doesn't fire
+        # after the exec.
+        signal.setitimer(signal.ITIMER_REAL, 0, 0)
+    # sys.path fixes: see comments at top of file.  If sys.path[0] is an empty
+    # string, we were (probably) invoked with -m and the effective path
+    # is about to change on re-exec.  Add the current directory to $PYTHONPATH
+    # to ensure that the new process sees the same path we did.
+    path_prefix = '.' + os.pathsep
+    if (sys.path[0] == '' and
+        not os.environ.get("PYTHONPATH", "").startswith(path_prefix)):
+        os.environ["PYTHONPATH"] = (path_prefix +
+                                    os.environ.get("PYTHONPATH", ""))
+    if sys.platform == 'win32':
+        # os.execv is broken on Windows and can't properly parse command line
+        # arguments and executable name if they contain whitespaces. subprocess
+        # fixes that behavior.
+        subprocess.Popen([sys.executable] + sys.argv)
+        sys.exit(0)
+    else:
+        try:
+            os.execv(sys.executable, [sys.executable] + sys.argv)
+        except OSError:
+            # Mac OS X versions prior to 10.6 do not support execv in
+            # a process that contains multiple threads.  Instead of
+            # re-executing in the current process, start a new one
+            # and cause the current process to exit.  This isn't
+            # ideal since the new process is detached from the parent
+            # terminal and thus cannot easily be killed with ctrl-C,
+            # but it's better than not being able to autoreload at
+            # all.
+            # Unfortunately the errno returned in this case does not
+            # appear to be consistent, so we can't easily check for
+            # this error specifically.
+            os.spawnv(os.P_NOWAIT, sys.executable,
+                      [sys.executable] + sys.argv)
+            sys.exit(0)
 
 @service.command(r"(?:windows )?update(?:s)?!?$", mention=True)
 @requires_permission("admin")
