602683374d5f30a36ca28002cfb56df60cf90ef7
Tony Young <tony@rfw.name>
2014-02-03 17:23:11 +1300

    [services/wolframalpha] make wolframalpha results more consistent

--- kochira/services/wolframalpha.py
+++ kochira/services/wolframalpha.py
@@ -26,22 +26,34 @@ def query(client, target, origin, query):
 
     result_node = tree.xpath("/queryresult[@success='true']")
 
-    if result_node:
-        result_node, = result_node
-        inp = " ".join(result_node.xpath("pod[@id='Input']/subpod[1]/plaintext/text()"))
-        primary = "\n".join(result_node.xpath("pod[@primary='true']/subpod[1]/plaintext/text()"))
-
-        result = inp + " = " + primary
-    else:
-        result = "(no result)"
-
-    prefix = ""
-
-    for line in result.split("\n"):
-        line = re.sub(r"(?<!\\)\\:([0-9a-fA-F]{4})", lambda x: chr(int(x.group(1), 16)), line)
-        client.message(target, "{origin}: {prefix} {line}".format(
-            origin=origin,
-            prefix=prefix,
-            line=line
+    if not result_node:
+        client.message(target, "{origin}: Couldn't compute that.".format(
+            origin=origin
         ))
-        prefix = "="
+        return
+
+    result_node, = result_node
+    inp = " ".join(result_node.xpath("pod[@id='Input']/subpod[1]/plaintext/text()")).strip()
+    primary = re.sub(
+        r"(?<!\\)\\:([0-9a-fA-F]{4})",
+        lambda x: chr(int(x.group(1), 16)),
+        "\n".join(result_node.xpath("pod[@primary='true']/subpod[1]/plaintext/text()")).strip()
+    ).split("\n")
+
+    if len(primary) > 1:
+        client.message(target, "{origin}: {inp}".format(
+            origin=origin,
+            inp=inp
+        ))
+
+        for line in primary:
+            client.message(target, "{origin}: | {line}".format(
+                origin=origin,
+                line=line
+            ))
+    else:
+        client.message(target, "{origin}: {inp} = {primary}".format(
+            origin=origin,
+            inp=inp,
+            primary=primary
+        ))
