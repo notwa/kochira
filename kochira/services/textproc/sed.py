73254fe11727f6d93ce9988705238586cee05015
Tony Young <tony@rfw.name>
2014-02-07 12:59:50 +1300

    [services/textproc/sed] add sedbot feature

---
+++ kochira/services/textproc/sed.py
@@ -0,0 +1,61 @@
+"""
+Sed-style find and replacement.
+
+Finds patterns in text and replaces it with other terms.
+
+Configuration Options
+=====================
+None.
+
+Commands
+========
+
+Find and Replace
+----------------
+
+::
+
+    s/<pattern>/<replacement>/<flags>
+    <who>: s/<pattern>/<replacement>/<flags>
+
+Find a regular expression pattern and replace it. Flags supported are `i` for
+case insensitive, `g` for global and `s` for dot-all.
+
+"""
+
+import re
+
+from kochira.service import Service
+
+service = Service(__name__, __doc__)
+
+
+@service.command(r"s(\W{1,2})(?P<pattern>(?:[^\1]|\\1)+)\1(?P<replacement>(?:[^\1]|\\1)+)\1(?P<flags>[gis]*)")
+@service.command(r"(?P<who>.+)[,;:] s(\W{1,2})(?P<pattern>(?:[^\2]|\\2)+)\2(?P<replacement>(?:[^\1]|\\2)+)\2(?P<flags>[gis]*)")
+def sed(client, target, origin, pattern, replacement, who=None, flags=None):
+    if flags is None:
+        flags = ""
+
+    re_flags = re.UNICODE
+
+    if "i" in flags:
+        re_flags |= re.IGNORECASE
+    if "s" in flags:
+        re_flags |= re.DOTALL
+
+    try:
+        expr = re.compile(pattern, re_flags)
+    except:
+        client.message(target, "{origin}: Couldn't parse that pattern.".format(
+            origin=origin
+        ))
+        return
+
+    for other, message in list(client.backlogs.get(target, []))[1:]:
+        if who is None or other == who:
+            match = expr.search(message)
+
+            if match is not None:
+                msg = expr.sub(replacement, message, count=0 if "g" in flags else 1)
+                client.message(target, "<{who}> {message}".format(who=other, message=msg))
+                break
