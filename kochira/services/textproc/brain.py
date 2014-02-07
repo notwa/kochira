c009f99f0662dec51ffc3205e14ab8c0ab17e692
Tony Young <tony@rfw.name>
2014-02-07 15:26:42 +1300

    [services/textproc/brain] add brain support

---
+++ kochira/services/textproc/brain.py
@@ -0,0 +1,59 @@
+"""
+Artificial (un)intelligence.
+
+Allows the bot to reply whenever its nickname is mentioned.
+
+Configuration Options
+=====================
+
+``brain_file``
+  Location to store the brain in.
+
+Commands
+========
+None.
+"""
+
+import re
+
+from kochira.service import Service, background
+from cobe.brain import Brain
+
+service = Service(__name__, __doc__)
+
+
+@service.setup
+def load_brain(bot):
+    config = service.config_for(bot)
+    storage = service.storage_for(bot)
+
+    storage.brain = Brain(config["brain_file"])
+
+
+@service.hook("channel_message", priority=-9999)
+@background
+def reply_and_learn(client, target, origin, message):
+    storage = service.storage_for(client.bot)
+
+    front, _, rest = message.partition(" ")
+
+    mention = False
+    reply = False
+
+    if front.strip(",:").lower() == client.nickname:
+        mention = True
+        reply = True
+        message = rest
+
+    if re.search(r"\b{}\b".format(re.escape(client.nickname)), message, re.I) is not None:
+        reply = True
+
+    if reply:
+        reply_message = storage.brain.reply(message)
+
+    if mention:
+        client.message(target, "{origin}: {message}".format(origin=origin, message=reply_message))
+    else:
+        client.message(target, reply_message)
+
+    storage.brain.learn(message)
