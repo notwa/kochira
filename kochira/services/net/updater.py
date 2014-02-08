a4bc65962074e768268b7de99f98fc8a644fe67c
Tony Young <tony@rfw.name>
2014-02-08 17:41:36 +1300

    [services/net/updater] trim extraneous lines in updater log

--- kochira/services/net/updater.py
+++ kochira/services/net/updater.py
@@ -71,7 +71,8 @@ def get_log(from_rev, to_rev):
     if p.returncode != 0:
         raise UpdateError("git log failed")
 
-    return out.decode("utf-8").rstrip("\n").split("\n")
+    return (line for line in out.decode("utf-8").rstrip("\n").split("\n")
+            if line)
 
 
 def do_update(remote, branch):
