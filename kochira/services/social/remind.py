774691bef3fb0dfcc95dfc3a9bc27dee8df02fe7
Tony Young <tony@rfw.name>
2014-02-10 21:07:56 +1300

    [services/social/remind] prioritize timed reminders over non-timed

--- kochira/services/social/remind.py
+++ kochira/services/social/remind.py
@@ -82,8 +82,8 @@ def play_timed_reminder(bot, reminder):
         reminder.delete_instance()
 
 
-@service.command(r"(?:remind|tell) (?P<who>\S+) (?:about|to|that) (?P<message>.+) (?P<duration>(?:in|after) .+|tomorrow)$", mention=True)
-@service.command(r"(?:remind|tell) (?P<who>\S+) (?P<duration>(?:in|after) .+|tomorrow) (?:about|to|that) (?P<message>.+)$", mention=True)
+@service.command(r"(?:remind|tell) (?P<who>\S+) (?:about|to|that) (?P<message>.+) (?P<duration>(?:in|after) .+|tomorrow)$", mention=True, priority=1)
+@service.command(r"(?:remind|tell) (?P<who>\S+) (?P<duration>(?:in|after) .+|tomorrow) (?:about|to|that) (?P<message>.+)$", mention=True, priority=1)
 def add_timed_reminder(client, target, origin, who, duration, message):
     """
     Add timed reminder.
