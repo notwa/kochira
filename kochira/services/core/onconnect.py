8c7f36a1a72ad9440c5fded9599045f5e6c894d9
Tony Young <tony@rfw.name>
2014-02-15 22:03:31 +1300

    [services/core/connect] rawmsg takes a list

--- kochira/services/core/onconnect.py
+++ kochira/services/core/onconnect.py
@@ -13,7 +13,7 @@ service = Service(__name__, __doc__)
 @service.config
 class Config(Config):
     commands = config.Field(doc="List of commands to run.",
-                            type=config.Many(str))
+                            type=config.Many(list))
 
 
 @service.hook("connect", priority=-10)
