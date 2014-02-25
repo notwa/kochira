d9e3ecfe7d7867c328c092c08ce3f6fd75145d30
Tony Young <tony@rfw.name>
2014-02-26 10:12:29 +1100

    [services/textproc/choose] commute choices. closes #21

--- kochira/services/textproc/choose.py
+++ kochira/services/textproc/choose.py
@@ -11,6 +11,11 @@ from kochira.service import Service
 
 service = Service(__name__, __doc__)
 
+def _ordering_key(k):
+    day = int(time.time() // (60 * 60 * 24))
+    return binascii.crc32(k.lower().encode("utf-8"), day)
+
+
 @service.command(r"choose (?P<options>.+)$", mention=True)
 @service.command(r"!choose (?P<options>.+)$")
 def choose(ctx, options):
@@ -21,9 +26,8 @@ def choose(ctx, options):
     """
 
     options = options.split(" or ")
-    options.sort()
+    options.sort(key=_ordering_key)
 
     ctx.respond(ctx._("I choose: {choice}").format(
-        choice=options[(binascii.crc32(" or ".join(options).lower().encode("utf-8")) +
-                        int(time.time() // (60 * 60 * 24))) % len(options)]
+        choice=options[-1]
     ))
