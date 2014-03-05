8e5f03be50cc98e1f61fe1d17e2cadaa9047473c
Tony Young <tony@rfw.name>
2014-03-05 21:22:53 +1100

    [services/textproc/unicode] add unicode thinger

---
+++ kochira/services/textproc/unicode.py
@@ -0,0 +1,60 @@
+"""
+Unicode character database.
+
+Identify unicode characters.
+"""
+
+import unicodedata
+
+from kochira.service import Service
+
+service = Service(__name__, __doc__)
+
+
+@service.command(r"!u (?P<character>.)", re_flags=0)
+def query(ctx, character):
+    """
+    Query.
+
+    Find information about a Unicode character.
+    """
+
+    try:
+        name = unicodedata.name(character)
+    except ValueError:
+        ctx.respond(ctx._("I don't know what that character is."))
+        return
+
+    category = unicodedata.category(character, "?")
+
+    ctx.respond(ctx._("{character}: {name} (Category: {category})").format(
+        character=character,
+        name=name,
+        category=category
+    ))
+
+
+@service.command(r"!U (?P<name>.+)", re_flags=0)
+def lookup(ctx, name):
+    """
+    Lookup.
+
+    Lookup a Unicode character by name.
+    """
+
+    name = name.upper()
+
+    try:
+        character = unicodedata.lookup(name)
+    except ValueError:
+        ctx.respond(ctx._("I don't know what that character is."))
+        return
+
+    category = unicodedata.category(character, "?")
+
+    ctx.respond(ctx._("{character}: {name} (Category: {category})").format(
+        character=character,
+        name=name,
+        category=category
+    ))
+
