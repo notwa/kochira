03fb45d5d0ce8ed87c82c73489aabb2d418ac4d8
Tony Young <tony@rfw.name>
2014-06-23 22:54:05 +1000

    better hugs

--- kochira/services/social/hug.py
+++ kochira/services/social/hug.py
@@ -14,12 +14,13 @@ def huggables(ctx):
 
 @service.hook("channel_message")
 def need_a_hug(ctx, target, origin, message):
+    front, _, rest = message.partition(" ")
     message = message.strip()
 
     k = (ctx.client.name, ctx.target)
 
     if any(t in message for t in [":(", ":-(", ":'(", "QQ", ":C", "T_T",
-                                  ";_;", ":c"]):
+                                  ";_;", ":c", ":<"]):
         ctx.client.ctcp(ctx.target,
                         "ACTION " + ctx._("hugs {who}").format(who=ctx.origin))
     elif any(t in message.lower() for t in ["hate this", "sad", "i need a hug",
@@ -27,7 +28,10 @@ def need_a_hug(ctx, target, origin, message):
                                             "i need a drink"]):
         ctx.respond("Need a hug?")
         ctx.storage.huggable.add(k)
-    elif message.lower() in ["yes", "yup", "yeah", "yep"] and \
+    elif front.strip(",:").lower() == ctx.client.nickname.lower() and \
+        rest.lower() in ["yes", "yup", "yeah", "yep"] and \
         k in ctx.storage.huggable:
-        ctx.respond(">--:)--<")
+        ctx.respond("(Ɔ˘⌣˘)(˘⌣˘C)")
         ctx.storage.huggable.remove(k)
+
+    return Service.EAT
