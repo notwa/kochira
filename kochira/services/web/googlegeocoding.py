3a76a99715d938dd448373af8bbb126f00643b2c
Tony Young <tony@rfw.name>
2014-12-23 19:00:47 +1300

    use more accurate distance

--- kochira/services/web/googlegeocoding.py
+++ kochira/services/web/googlegeocoding.py
@@ -4,8 +4,8 @@ Geocoding.
 Look up and reverse look up addresses.
 """
 
-import math
 import requests
+from geopy.distance import vincenty
 
 from kochira import config
 from kochira.service import Service, background, Config, coroutine
@@ -204,12 +204,6 @@ def nearby_search(ctx, what, where=None, radius : int=None, num : int=None):
     ))
 
 
-def haversin(theta):
-    return (1 - math.cos(theta)) / 2
-
-EARTH_RADIUS = 6367.5
-
-
 @service.command(r"distance to (?P<end_loc>.+?) from (?P<start_loc>.+?)", mention=True, priority=1)
 @service.command(r"distance(?: from (?P<start_loc>.+?))? to (?P<end_loc>.+?)", mention=True)
 @background
@@ -234,8 +228,6 @@ def distance(ctx, end_loc, start_loc=None):
 
     start_result = start_results[0]
     start_coords = start_result["geometry"]["location"]
-    rlat1, rlng1 = \
-        math.radians(start_coords["lat"]), math.radians(start_coords["lng"])
 
     end_results = yield ctx.provider_for("geocode")(end_loc)
 
@@ -247,15 +239,10 @@ def distance(ctx, end_loc, start_loc=None):
 
     end_result = end_results[0]
     end_coords = end_result["geometry"]["location"]
-    rlat2, rlng2 = \
-        math.radians(end_coords["lat"]), math.radians(end_coords["lng"])
-
-    d = 2 * EARTH_RADIUS * math.asin(
-        math.sqrt(haversin(rlat2 - rlat1) +
-                  math.cos(rlat1) * math.cos(rlat2) * haversin(rlng2 - rlng1)))
 
     ctx.respond(ctx._("Distance from {start} to {end}: {distance:.3f} km").format(
         start=start_result["formatted_address"],
         end=end_result["formatted_address"],
-        distance=d
+        distance=vincenty((start_coords["lat"], start_coords["lng"]),
+                          (end_coords["lat"], end_coords["lng"]))
     ))
