ca7764ae410a1f3f9b460727214d9bd9f3333c03
Tony Young <tony@rfw.name>
2015-02-18 15:22:14 +1100

    Create wikipedia.py

---
+++ kochira/services/web/wikipedia.py
@@ -0,0 +1,40 @@
+"""
+Wikipedia.
+
+Extract a Wikipedia excerpt for the given query.
+"""
+
+import bs4
+import requests
+
+from kochira.service import Service, background
+
+service = Service(__name__, __doc__)
+
+@service.command(r"!wiki (?<term>.+)")
+@background
+def lookup(ctx, term):
+    """
+    Look up.
+
+    Look up the article.
+    """
+
+    r = requests.get("http://en.wikipedia.org/w/api.php", params={
+        "format": "json",
+        "action": "query",
+        "prop": "extract|info",
+        "inprop": "url",
+        "rawcontinue": "true",
+        "titles": term
+    }).json()
+
+    page, = r["query"]["pages"].values()
+    
+    if "missing" in page:
+        ctx.respond(ctx._("Couldn't find that."))
+        return
+
+    text, *_ = bs4.BeautifulSoup(page["extract"]).get_text().split("\n")
+
+    ctx.respond(ctx._("{url}: {text}").format(link=page["canonicalurl"], text=text))
