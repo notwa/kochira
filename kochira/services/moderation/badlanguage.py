6c99629be0b3c7a6ea41d99c520bfea5bf880e6e
Tony Young <tony@rfw.name>
2015-07-30 15:13:03 -0700

    debuggin

--- kochira/services/moderation/badlanguage.py
+++ kochira/services/moderation/badlanguage.py
@@ -29,6 +29,7 @@ class Config(Config):
 @service.hook("channel_message", priority=2500)
 def check_bad_languages(ctx, target, origin, message):
     resp = requests.get("http://ws.detectlanguage.com/0.2/detect", params={"q": message, "key": ctx.config.api_key}).json()
+    raise Exception(resp)
     if any(detection in ctx.config.languages for detection in resp["data"]["detections"] if detection["isReliable"]):
         ctx.client.rawmsg("KICK", ctx.target, ctx.origin, ctx.config.kick_message)
         return Service.EAT
